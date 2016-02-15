d24c4c347f6accaa704a33fee2dd0c64a9d283a2
Just van den Broecke <just@justobjects.nl>
2016-02-15 16:51:32 +0100

    save updates smartapp4

--- app/smartapp/map.js
+++ app/smartapp/map.js
@@ -1,98 +1,86 @@
-$(document).ready(function() {
-		
-	//	$('table').hide();
-	//	$('button').click(function() {
-	//	$("table").toggle();
-	//	});
-				
+$(document).ready(function () {
+
+    //	$('table').hide();
+    //	$('button').click(function() {
+    //	$("table").toggle();
+    //	});
+
     // create the tile layer with correct attribution
-    var map = new L.Map('map', {zoom: 13, center: new L.latLng([51.8348, 5.85]) });
+    var map = new L.Map('map', {zoom: 13, center: new L.latLng([51.8348, 5.85])});
     var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
     var osmTiles = new L.TileLayer(osmUrl, {attribution: osmAttrib});
     map.addLayer(osmTiles);
-	var source  = $("#entry-template").html();
-	var template = Handlebars.compile(source);
+    var source = $("#entry-template").html();
+    var template = Handlebars.compile(source);
 
-	// See http://stackoverflow.com/questions/11916780/changing-getjson-to-jsonp
+    // See http://stackoverflow.com/questions/11916780/changing-getjson-to-jsonp
     // Notice the callback=? . This triggers a JSONP call
-	
-	var locaties = 'http://api.smartemission.nl/sosemu/api/v1/stations?format=json&callback=?';
-		$.getJSON(locaties, function (data) {
-			var geojson = L.geoJson(data).addTo(map)
 
-				.on('click', function (e) {
-					var stationId = e.layer.feature.properties.id;
-					var timeseriesUrl = 'http://api.smartemission.nl/sosemu/api/v1/timeseries?station=' + stationId + '&callback=?';
+    var locaties = 'http://api.smartemission.nl/sosemu/api/v1/stations?format=json&callback=?';
+    $.getJSON(locaties, function (data) {
+        var geojson = L.geoJson(data).addTo(map)
 
-					$.getJSON(timeseriesUrl, function (data) {
-						var html = template(data);
-						// Hier met JQUery
-						var sidebarElm = $("#sidebar");
-						sidebarElm.append(html);
-						sidebar.toggle();
-					});
-				});
-	});
+            .on('click', function (e) {
+                var stationId = e.layer.feature.properties.id;
+                var timeseriesUrl = 'http://api.smartemission.nl/sosemu/api/v1/timeseries?station=' + stationId + '&callback=?';
 
-	var value = 'var values = "http://api.smartemission.nl/sosemu/api/v1/timeseries?station=23&callback=?';
-		//$.getJSON(value, function (data) {
-// naam van het station, regel 38 van HTML. 		
-			//var name = L.geoJson(data).addTo(map)
-			//{
-				//.on('click', function (feature, layer) {
-					//layer(feature.properties.label);
-			   //});
-		    //};
-// coordinaten van het station, regel 49 HTML		
-			//var coordinate = L.geoJson(data).addTo(map)
-			//{
-				//onEachFeature: function (feature, layer) {
-					//layer(feature.geometry.coordinate);
-				//};
-			//};
-		//});
-	
-	
-	
-		var sidebar = L.control.sidebar('sidebar', {
-			closeButton: true,
-			position: 'left'
-		}); 	
-		map.addControl(sidebar);
-					
-		var locatie = L.icon({
-				iconUrl: 'locatie-icon.png',
-				iconSize:     	[24, 41],
-				iconAnchor:		[10, 40]
-			});
-		
-		var locatieclick = L.icon({
-				iconUrl: 'locatie-icon-click.png',
-				iconSize:     [24, 41], 
-				iconAnchor:   [10, 40]
-			});
+                $.getJSON(timeseriesUrl, function (data) {
+                    stationData = {
+                        station : e.layer.feature,
+                        data : data
+                    };
 
-		
-		map.on('click', function () {
-			sidebar.hide();
-			map.setView([51.8348, 5.85],13);  
-		});
-		sidebar.on('show', function () {	
-			console.log('Sidebar will be visible.');
-		});
-		sidebar.on('shown', function () {						
-			console.log('Sidebar is visible.');
-		});
-		sidebar.on('hide', function () { 						
-			console.log('Sidebar will be hidden.');
-		});
-		sidebar.on('hidden', function () { 						
-			console.log('Sidebar is hidden.');
-		});
-		
-		sidebar.on(sidebar.getCloseButton(), 'click', function () {					
-			console.log('Close button clicked.');
-		});
-		
+                    var html = template(stationData);
+
+                    // Hier met JQuery
+                    var sidebarElm = $("#sidebar");
+                    // sidebarElm clearen
+                    sidebarElm.append(html);
+                    sidebar.toggle();
+                });
+            });
+    });
+
+
+    var sidebar = L.control.sidebar('sidebar', {
+        closeButton: true,
+        position: 'left'
+    });
+    map.addControl(sidebar);
+
+    var locatie = L.icon({
+        iconUrl: 'locatie-icon.png',
+        iconSize: [24, 41],
+        iconAnchor: [10, 40]
+    });
+
+    var locatieclick = L.icon({
+        iconUrl: 'locatie-icon-click.png',
+        iconSize: [24, 41],
+        iconAnchor: [10, 40]
+    });
+
+
+    map.on('click', function () {
+        sidebar.hide();
+        map.setView([51.8348, 5.85], 13);
+    });
+    sidebar.on('show', function () {
+        console.log('Sidebar will be visible.');
+    });
+    sidebar.on('shown', function () {
+        console.log('Sidebar is visible.');
+    });
+    sidebar.on('hide', function () {
+        console.log('Sidebar will be hidden.');
+    });
+    sidebar.on('hidden', function () {
+        console.log('Sidebar is hidden.');
+    });
+
+    sidebar.on(sidebar.getCloseButton(), 'click', function () {
+        console.log('Close button clicked.');
+    });
+
 });
