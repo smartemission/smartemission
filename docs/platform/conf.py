809cb8d0ef0160b7024138b5dbe1d4b5385cbb98
Just van den Broecke <just@justobjects.nl>
2016-04-05 16:16:31 +0200

    first stab at tech platform doc

--- docs/platform/conf.py
+++ docs/platform/conf.py
@@ -230,7 +230,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'smartplat', u'Smart Emission Data Platform Documentation',
+    ('index', 'smartplatform', u'Smart Emission Data Platform Documentation',
      [u'Thijs Brentjens, Just van den Broecke, Michel Grothe'], 1)
 ]
 
