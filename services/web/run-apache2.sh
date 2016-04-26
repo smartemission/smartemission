6a6a8ab17ac03d556c5faeef46041b98978562da
Just van den Broecke <just@justobjects.nl>
2016-04-26 14:38:07 +0200

    refactor dirs step2

--- services/web/run-apache2.sh
+++ services/web/run-apache2.sh
@@ -12,6 +12,7 @@ IMAGE="geonovum/apache2"
 
 VOL_MAP="-v ${CONFIG}/sites-enabled:/etc/apache2/sites-enabled -v ${GIT}:${GIT} -v ${LOG}/apache2:/var/log/apache2"
 PORT_MAP="-p 2222:22 -p 80:80"
+LINK_MAP="--link ${PG_HOST}:${PG_HOST}"
 PG_HOST="postgis"
 
 # Stop and remove possibly old containers
@@ -19,4 +20,4 @@ sudo docker stop ${NAME} > /dev/null 2>&1
 sudo docker rm ${NAME} > /dev/null 2>&1
 
 # Finally run
-sudo docker run --name ${NAME} --link ${PG_HOST}:${PG_HOST} -d ${PORT_MAP} ${VOL_MAP} -t -i ${IMAGE}
+sudo docker run -d --name ${NAME} ${LINK_MAP} ${PORT_MAP} ${VOL_MAP} -t -i ${IMAGE}
