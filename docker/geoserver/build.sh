2074fc686d18aca867bce3e3a479419c827d255f
Just van den Broecke <just@justobjects.nl>
2016-06-01 14:15:24 +0200

    upgrade GeoServer to 2.9.0, requires Tomcat 8.5.2 which requires Java8, usual Java-mess :-(

--- docker/geoserver/build.sh
+++ docker/geoserver/build.sh
@@ -2,6 +2,6 @@
 #
 # Build geoserver Docker image with options
 
-sudo docker build --build-arg IMAGE_TIMEZONE="Europe/Amsterdam" --build-arg TOMCAT_EXTRAS=false --build-arg ORACLE_JDK=false  -t geonovum/geoserver .
+sudo docker build --build-arg IMAGE_TIMEZONE="Europe/Amsterdam" --build-arg TOMCAT_EXTRAS=false --build-arg ORACLE_JDK=false  -t geonovum/geoserver:2.9.0 .
 
 # sudo docker build -t geonovum/geoserver .
--- services/geoserver/run-geoserver.sh
+++ services/geoserver/run-geoserver.sh
@@ -8,9 +8,10 @@ LOG="/var/smartem/log"
 TC_LOG="${LOG}/tomcat-geoserver"
 NAME="geoserver"
 # IMAGE="kartoza/geoserver:2.8.0"
-IMAGE="geonovum/geoserver"
+IMAGE="geonovum/geoserver:2.9.0"
 PG_HOST="postgis"
-DATA_DIR="/var/smartem/data/geoserver"
+DATA_DIR="/var/smartem/data"
+GS_DATA_DIR="${DATA_DIR}/geoserver"
 
 # First time: may make emtpy dirs. Docker will fill these once with
 # values in container and leave them in subsequent runs. Hence the GeoServer
@@ -21,7 +22,8 @@ DATA_DIR="/var/smartem/data/geoserver"
 # sudo mkdir -p ${TC_LOG}
 
 # Define the mappings for local dirs, ports and PostGIS Docker container
-VOL_MAP="-v ${DATA_DIR}:/opt/geoserver/data_dir -v ${TC_LOG}:/usr/local/tomcat/logs"
+VOL_MAP="-v ${GS_DATA_DIR}:/opt/geoserver/data_dir -v ${TC_LOG}:/usr/local/tomcat/logs"
+
 # PORT_MAP="-p 8080:8080"
 PORT_MAP=""
 LINK_MAP="--link ${PG_HOST}:${PG_HOST}"
@@ -33,6 +35,20 @@ sudo docker rm ${NAME} > /dev/null 2>&1
 # Finally run with all mappings
 sudo docker run --name ${NAME} ${LINK_MAP} ${PORT_MAP} ${VOL_MAP} -d ${IMAGE}
 
+if [ "$(ls -A $GS_DATA_DIR)" ]; then
+   echo "Ok, using existing GeoServer data dir: $GS_DATA_DIR on host"
+else
+   echo "$GS_DATA_DIR empty: copy from original GS version and restart GS"
+   # Copy from original (somehow mapping render an empty dir!)
+   sudo docker cp geoserver:/opt/geoserver/data_dir.orig $DATA_DIR
+   sudo rm -rf $GS_DATA_DIR
+   sudo mv  ${DATA_DIR}/data_dir.orig $GS_DATA_DIR
+
+   # Rerun
+   sudo docker stop ${NAME} > /dev/null 2>&1
+   sudo docker rm ${NAME} > /dev/null 2>&1
+   sudo docker run --name ${NAME} ${LINK_MAP} ${PORT_MAP} ${VOL_MAP} -d ${IMAGE}
+fi
+
 # When running: get into container with bash
 # sudo docker exec -it geoserver  bash
-
