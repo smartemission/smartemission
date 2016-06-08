02e65e25d0d55ff4a2ab7a98b9b665706459a606
Just van den Broecke <just@justobjects.nl>
2016-06-08 20:48:28 +0200

    first raw version 52North SOS Docker image and container

---
+++ docker/sos52n/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Build geoserver Docker image with options
+
+sudo docker build --build-arg IMAGE_TIMEZONE="Europe/Amsterdam" --build-arg TOMCAT_EXTRAS=true --build-arg ORACLE_JDK=false  -t geonovum/sos52n:4.3.6 .
+
+# sudo docker build -t geonovum/geoserver .
--- services/sos52n/run.sh
+++ services/sos52n/run.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 #
-# Run the Postgresql server with PostGIS and default database "gis".
+# Run the 52NorthSOS Docker image with local and PostGIS Docker mappings.
 #
 
 GIT="/opt/geonovum/smartem/git"
 LOG="/var/smartem/log"
+TC_LOG="${LOG}/tomcat-sos52n"
 NAME="sos52n"
-IMAGE="geonovum/sos52n"
+IMAGE="geonovum/sos52n:4.3.6"
+PG_HOST="postgis"
 
-# VOL_MAP="-v /var/smartem/data/postgresql:/var/lib/postgresql -v ${LOG}/postgresql:/var/log/postgresql"
-# PORT_MAP="-p 5432:5432"
+# Define the mappings for local dirs, ports and PostGIS Docker container
+# VOL_MAP_DATA="-v ${GS_DATA_DIR}:/opt/sos52n/data_dir"
+VOL_MAP_LOGS="-v ${TC_LOG}:/usr/local/tomcat/logs"
+VOL_MAP="${VOL_MAP_LOGS}"
+
+# If we need to expose 8080 from host, but we use Apache AJP
+# PORT_MAP="-p 8080:8080"
 PORT_MAP=""
+LINK_MAP="--link ${PG_HOST}:${PG_HOST}"
 
+echo "Start ${NAME} with volumes: ${VOL_MAP}"
 # Stop and remove possibly old containers
-# sudo docker stop ${NAME} > /dev/null 2>&1
-# sudo docker rm ${NAME} > /dev/null 2>&1
+sudo docker stop ${NAME} > /dev/null 2>&1
+sudo docker rm ${NAME} > /dev/null 2>&1
+# Finally run with all mappings
+sudo docker run --name ${NAME} ${LINK_MAP} ${PORT_MAP} ${VOL_MAP} -d ${IMAGE}
 
-# Finally run
-# sudo docker run --name ${NAME} ${PORT_MAP} ${VOL_MAP} -d -t ${IMAGE}
-
+# When running: get into container with bash
+# sudo docker exec -it sos52n  bash
