b7f5e01afdfa430c6dbab1a1d1aaa8c0b60eb482
Just van den Broecke <just@justobjects.nl>
2016-07-23 13:47:52 +0200

    Legenda OpenTopo maps

--- services/web/site/heron/config/Map.js
+++ services/web/site/heron/config/Map.js
@@ -325,7 +325,7 @@ Heron.options.map.layers = [
             metadata: {
                 legend: {
                     // Use a fixed URL as legend
-                    legendURL: 'images/legend/legenda-opentopo-1012.png',
+                    legendURL: 'images/legend/legenda-opentopo-760.png',
                     hideInLegend: false
                 }
             }
