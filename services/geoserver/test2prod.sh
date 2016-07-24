40c298b2dd6be1a5f2a88ef56b847057416f0c37
Just van den Broecke <just@justobjects.nl>
2016-07-24 15:40:30 +0200

    sync scripts geoserver and sos52n

--- services/geoserver/test2prod.sh
+++ services/geoserver/test2prod.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Sync SOS data/config to production
+# Sync GeoServer data/config to production
 
-dir=/var/smartem/data/sos52n/
+dir=/var/smartem/data/geoserver/
 phost=root@data.smartemission.nl
 
 rsync -e ssh -alzvx --delete ${dir} ${phost}:${dir}
