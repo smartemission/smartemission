6d0cf2b528d1bd114af8acbe043623f340232687
Pieter Marsman <pietermarsman@gmail.com>
2016-10-14 18:44:21 +0200

    New final parameters for No2

--- etl/calibration/src/python/params.py
+++ etl/calibration/src/python/params.py
@@ -41,11 +41,20 @@ O3_final = {'mlp__activation': ['logistic'],
             'mlp__hidden_layer_sizes': [141], 'mlp__max_iter': [200],
             'mlp__solver': ['lbfgs']}
 
-NO2_final = {'mlp__learning_rate': ['constant'], 'mlp__activation': ['tanh'],
-             'mlp__alpha': [1.4312431413452899e-05],
-             'mlp__learning_rate_init': [4.2639692257501962e-05],
-             'mlp__hidden_layer_sizes': [71], 'mlp__max_iter': [200],
-             'mlp__solver': ['lbfgs'], 'filter__alpha': [0.01358001]}
+# NO2_final = {'mlp__learning_rate': ['constant'], 'mlp__activation': ['tanh'],
+#              'mlp__alpha': [1.4312431413452899e-05],
+#              'mlp__learning_rate_init': [4.2639692257501962e-05],
+#              'mlp__hidden_layer_sizes': [71], 'mlp__max_iter': [200],
+#              'mlp__solver': ['lbfgs'], 'filter__alpha': [0.01358001]}
+NO2_final = {'mlp__max_iter': [10000], 'mlp__nesterovs_momentum': [True],
+             'mlp__alpha': [0.001350850267756745],
+             'mlp__early_stopping': [True],
+             'mlp__momentum': [0.41926154492362788],
+             'mlp__hidden_layer_sizes': [(33, 16)], 'mlp__solver': ['sgd'],
+             'mlp__learning_rate': ['adaptive'],
+             'mlp__learning_rate_init': [0.027842603578321252],
+             'filter__alpha': [0.0097972427184488658],
+             'mlp__batch_size': ['auto'], 'mlp__activation': ['tanh']}
 
 dist_test = dist_01.copy()
 dist_test['mlp__hidden_layer_sizes'] = randint(2, 10)
