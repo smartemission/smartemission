377df6058cd842380eedab5a7f3bab596d8ff67f
Just van den Broecke <just@justobjects.nl>
2016-11-07 20:59:24 +0100

    db restore script improved

--- platform/restore-db.sh
+++ platform/restore-db.sh
@@ -2,7 +2,7 @@
 # restore van een complete DB
 # Just van den Broecke 2016
 #
-# USAGE ./restore-db.sh <pad naar .bz2 dump bestand>
+# USAGE ./restore-db.sh <pad naar dump bestand>
 
 # Databases
 export PGDB=gis
@@ -12,15 +12,31 @@ export PGHOST=`sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' p
 
 if [ $# -eq 0 ]
 then
-    echo "Geef .bz2 DB dump bestand bestand op als parameter."
+    echo "Geef DB dump bestand bestand op als parameter."
     exit 1
 fi
 
-if [ ! -f $1 ]
+PGDUMPFILE=$1
+
+if [ ! -f $PGDUMPFILE ]
 then
-    echo "Bestand $1 niet gevonden."
+    echo "Bestand $PGDUMPFILE niet gevonden."
     exit 1
 fi
 
 
-bzip2 -c -d $1 | psql -h ${PGHOST} -U ${PGUSER} ${PGDB}
+# bzip2 -c -d $1 | psql -h ${PGHOST} -U ${PGUSER} ${PGDB}
+
+function __restore {
+
+	# Restore data (db or schema) from dump file
+	__msg "Restoring $PGDUMPFILE in db $PGDB (duurt even...)"
+	cmd="pg_restore -v -d $PGDB --no-owner --clean -Fc -U $PGUSER -h $PGHOST $PGDUMPFILE"
+	__msg "execute: "$cmd""
+	$cmd
+
+	# Basic Check
+	__msg "bestand $PGDUMPFILE ingelezen..."
+}
+
+__restore
