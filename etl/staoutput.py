52a5971bfbe813411c0cbddcef3c58ccfa3c9185
Just van den Broecke <just@justobjects.nl>
2016-11-14 18:06:10 +0100

    #26 first very rough version STA Publisher with working POSTs!

--- etl/staoutput.py
+++ etl/staoutput.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Output classes for ETL.
+# Output classes for ETL with SensorThings API.
 #
 # Author: Just van den Broecke
 #
 from os import sys, path
 from stetl.outputs.httpoutput import HttpOutput
+from urllib2 import Request, urlopen, URLError, HTTPError
+import urllib
+import json
+from sensordefs import *
+
 from stetl.util import Util
 from stetl.packet import FORMAT
 from stetl.component import Config
@@ -14,12 +19,13 @@ log = Util.get_log('staoutput')
 
 class STAOutput(HttpOutput):
     """
-    Output via SOS-T protocol over plain HTTP.
+    Output via SensorThings API (STA) over plain HTTP.
+    See examples: http://www.sensorup.com/docs/?python
 
     consumes=FORMAT.record_array
     """
 
-    @Config(ptype=str, default='application/json;charset=UTF-8', required=True)
+    @Config(ptype=str, default='application/json;charset=UTF-8', required=False)
     def content_type(self):
         """
         The content type (for template).
@@ -45,156 +51,172 @@ class STAOutput(HttpOutput):
         HttpOutput.__init__(self, configdict, section, consumes=FORMAT.record_array)
 
         # Template file, to be used as POST body with substituted values
+        # TODO use Jinja2 formatting i.s.o. basic string formatting
+
         self.post_thing_templ_path = '%s/thing.json' % self.template_file_root
         self.post_datastream_templ_path = '%s/datastream.json' % self.template_file_root
         self.post_observation_templ_path ='%s/observation.json' % self.template_file_root
-        self.proc_desc_templ_path = '%s/procedure-desc.xml' % self.template_file_root
         self.post_thing_templ_str = None
+        self.post_datastream_templ_str = None
         self.post_observation_templ_str = None
         self.base_path = self.path
+        self.base_url = 'http://%s:%d%s' % (self.host, self.port, self.path)
 
-    def init(self):
-        # read the post_observation template once
+    def init_templates(self):
+        # read all POST payload templates once
+
+        log.info('Init: read template file: %s' % self.post_thing_templ_path)
+        with open(self.post_thing_templ_path, 'r') as f:
+            self.post_thing_templ_str = f.read()
+
+        log.info('Init: read template file: %s' % self.post_datastream_templ_path)
+        with open(self.post_datastream_templ_path, 'r') as f:
+            self.post_datastream_templ_str = f.read()
+
         log.info('Init: read template file: %s' % self.post_observation_templ_path)
         with open(self.post_observation_templ_path, 'r') as f:
             self.post_observation_templ_str = f.read()
 
-        # For insert-sensor we need the Procedure SML (XML) and escape/insert this into
-        # the JSON insert-sensor string.
-        # log.info('Init: read template file: %s' % self.proc_desc_templ_path)
-        # with open(self.proc_desc_templ_path, 'r') as f:
-        #     proc_desc = f.read()
-        #     proc_desc = proc_desc.replace('"', '\\"').replace('\n', '')
 
-        log.info('Init: read template file: %s' % self.post_thing_templ_path)
-        with open(self.post_thing_templ_path, 'r') as f:
-            post_thing_str = f.read()
-            # self.post_thing_templ_str = post_thing_str.replace('{procedure-desc.xml}', proc_desc)
-            self.post_thing_templ_str = post_thing_str
+    def init_things(self):
+        log.info('Init: read all Things')
+
+        things_rsp = self.read_from_url(self.base_url + '/Things')
+        self.things_list = things_rsp['value']
+        self.things = {}
+        for thing in self.things_list:
+            id = thing['properties']['id']
+            thing['datastreams'] = {}
+            self.things[id] = thing
+
+    def init(self):
+        self.init_templates()
+        self.init_things()
+
+    def post_thing(self, record):
+        format_args = dict()
+
+        format_args['station_id'] = record['device_id']
+        # format_args['station_altitude'] = record['altitude']
+        format_args['station_lon'] = record['lon']
+        format_args['station_lat'] = record['lat']
+
+        payload = self.post_thing_templ_str.format(**format_args)
+        self.path = self.base_path + '/Things'
+        statuscode, statusmessage, res = HttpOutput.post(self, None, payload)
+        thing = json.loads(res)
+        return thing
+
+    def post_datastream(self, thing, record):
+        # {'device_id': 1, 'name': 'temperature', 'value': 19, 'unit': 'Celsius', 'gid': 50,
+        # 'time': datetime.datetime(2016, 4, 27, 5, 0, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=120, name=None)),
+        # 'lat': 51.472585, , 'lon': 5.671208, , 'altitude': 210, }
+        format_args = dict()
+        sensor_def = SENSOR_DEFS[record['name']]
+
+        format_args['thing_id'] = thing['@iot.id']
+        format_args['station_id'] = record['device_id']
+        format_args['name'] = record['name']
+        format_args['label'] = sensor_def['label']
+        format_args['unit'] = record['unit']
+
+        payload = self.post_datastream_templ_str.format(**format_args)
+        self.path = self.base_path + '/Datastreams'
+        statuscode, statusmessage, res = HttpOutput.post(self, None, payload)
+        datastream = json.loads(res)
+        return datastream
+
+    def read_from_url(self, url, parameters=None):
+        """
+        Read the data from the URL.
+        :param url: the url to fetch
+        :param parameters: optional dict of query parameters
+        :return:
+        """
+        # log.info('Fetch data from URL: %s ...' % url)
+        req = Request(url)
+        req.add_header('Content-Type', self.content_type)
+        req.add_header('Accept', self.content_type)
+        json_response = None
+        try:
+            # Urlencode optional parameters
+            query_string = None
+            if parameters:
+                query_string = urllib.urlencode(parameters)
+
+            response = urlopen(req, query_string)
+            response_str = response.read()
+            json_response = json.loads(response_str)
+        except HTTPError as e:
+            log.error('HTTPError fetching from URL %s: code=%d e=%s' % (url, e.code, e))
+        except URLError as e:
+            log.error('URLError fetching from URL %s: reason=%s e=%s' % (url, e.reason, e))
+
+        # everything is fine
+        return json_response
 
     def post(self, packet, payload):
         # {'device_id': 1, 'name': 'temperature', 'value': 19, 'unit': 'Celsius', 'gid': 50,
         # 'time': datetime.datetime(2016, 4, 27, 5, 0, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=120, name=None)),
         # 'lat': 51.472585, , 'lon': 5.671208, , 'altitude': 210, }
         record = packet.data
-        device_id = record['device_id']
         component = record['name']
+        device_id = str(record['device_id'])
         gid = record['gid']
         id = '%s-%s-%s' % (device_id, component, gid)
+
         log.info('====START POST Observation id=%s' % id)
-        log.info('POSTing Observation! try 1 - payload=%s' % payload)
 
         statuscode, statusmessage, res = HttpOutput.post(self, packet, payload)
 
         # InsertObservation may fail when Sensor not in SOS
         # Try to do an InsertSensor and try InsertObservation again
-        if statuscode == 400:
-            log.info('No sensor for station: res=%s, will insert' % res)
-            post_thing_payload = self.create_post_thing_payload(packet)
-            log.info('POSTing InsertSensor! - payload=%s' % post_thing_payload)
-            statuscode, statusmessage, res = HttpOutput.post(self, packet, post_thing_payload)
-            if statuscode != 200:
-                log.warn('FAIL InsertSensor for station: rec=%s res=%s' % (str(packet.data), res))
-            else:
-                log.info('YES InsertSensor OK! id=%s POSTing InsertObservation! try 2' % id)
-                statuscode, statusmessage, res = HttpOutput.post(self, packet, payload)
-                if statuscode == 200:
-                    log.info('YES InsertObservation! try 2 OK!! %s' % id)
-                else:
-                    log.warn('FAIL InsertObservation payload=%s res=%s' % (payload, res))
-        elif statuscode == 200:
-            log.info('YES inserted Observation! try 1 id=%s' % id)
+        if statuscode in [200, 201]:
+            log.info('YES inserted Observation! id=%s status=%s' % (id, statusmessage))
+        else:
+            log.warn('FAIL InsertObservation status=%d payload=%s res=%s' % (statuscode, payload, res))
 
         log.info('====END InsertObservation id=%s' % id)
 
         return statuscode, statusmessage, res
 
-    def create_post_thing_payload(self, packet):
-        # String substitution based on Python String.format()
-        # <local_id>STA-NL00807</local_id>
-        # <natl_station_code>807</natl_station_code>
-        # <eu_station_code>NL00807</eu_station_code>
-        # <name>Hellendoorn-Luttenbergerweg</name>
-        # <municipality>Hellendoorn</municipality>
-        # <altitude>7</altitude>
-        # <altitude_unit>m</altitude_unit>
-        # <area_classification>http://dd.eionet.europa.eu/vocabulary/aq/areaclassification/rural</area_classification>
-        # <activity_begin>1976-04-02T00:00:00+01:00</activity_begin>
-        # <activity_end></activity_end>
-        # <version></version>
-        # <belongs_to></belongs_to>
-        # <lon></lon>
-        # <lat></lat>
-        record = packet.data
-        format_args = dict()
-        format_args['station_id'] = record['device_id']
-        format_args['station_name'] = record['device_id']
-        format_args['station_altitude'] = record['altitude']
-        format_args['station_lon'] = record['lon']
-        format_args['station_lat'] = record['lat']
-
-        payload = self.post_thing_templ_str.format(**format_args)
-        return payload
-
     def create_payload(self, packet):
         record = packet.data
-        format_args = dict()
-        # need station_id, unique_id (sample_id?),
-        # component, municipality(may be null), station_lat, station_lon,
-        # sample_time, sample_value
-        # format_args['component'] = record['name']
-        # format_args['station_id'] = record['device_id']
+        device_id = str(record['device_id'])
+        thing = self.things.get(device_id)
+        ds_name = record['name']
+        if not thing:
+            thing = self.post_thing(record)
+            thing['datastreams'] = {}
+            datastream = self.post_datastream(thing, record)
+            thing['datastreams'][ds_name] = datastream
+            self.things[device_id] = thing
 
-        # See issue: somehow the unique_id ends up in the capabilities doc!
-        # format_args['unique_id'] = '%s-%s' % (record['device_id'], record['gid'])
+        datastream = thing['datastreams'].get(ds_name)
+        if not datastream:
+            ds_resp = self.read_from_url(self.base_url + '/Things(%d)/Datastreams?$expand=ObservedProperty' % thing['@iot.id'])
+            ds_list = ds_resp['value']
+            for ds in ds_list:
+                ds_name = ds['ObservedProperty']['name']
+                thing['datastreams'][ds_name] = ds
+
+            datastream = thing['datastreams'].get(ds_name)
+            if not datastream:
+                datastream = self.post_datastream(thing, record)
+                thing['datastreams'][ds_name] = datastream
+
+        format_args = dict()
 
         # Time format: "yyyy-MM-dd'T'HH:mm+0N00"  e.g. 2013-09-29T18:46:19+0100
         t = record['time']
         t_offset = t.tzinfo._offset.seconds / 3600
         format_args['sample_time'] = t.strftime('%Y-%m-%dT%H:%M:%S' + '+0%d00' % t_offset)
-        # format_args['station_lon'] = record['lon']
-        # format_args['station_lat'] = record['lat']
         format_args['sample_value'] = record['value']
-        format_args['foi_id'] = record['device_id']
-        format_args['datastream_id'] = record['device_id']
+        format_args['datastream_id'] = datastream['@iot.id']
 
-        # TODO use Jinja2 formatting
         payload = self.post_observation_templ_str.format(**format_args)
 
         # REST: post to collection
         self.path = self.base_path + '/Observations'
-
-        # if self.sos_request == 'insert-sensor':
-        #
-        #     # String substitution based on Python String.format()
-        #     # <local_id>STA-NL00807</local_id>
-        #     # <natl_station_code>807</natl_station_code>
-        #     # <eu_station_code>NL00807</eu_station_code>
-        #     # <name>Hellendoorn-Luttenbergerweg</name>
-        #     # <municipality>Hellendoorn</municipality>
-        #     # <altitude>7</altitude>
-        #     # <altitude_unit>m</altitude_unit>
-        #     # <area_classification>http://dd.eionet.europa.eu/vocabulary/aq/areaclassification/rural</area_classification>
-        #     # <activity_begin>1976-04-02T00:00:00+01:00</activity_begin>
-        #     # <activity_end></activity_end>
-        #     # <version></version>
-        #     # <belongs_to></belongs_to>
-        #     # <lon></lon>
-        #     # <lat></lat>
-        #     format_args = dict()
-        #     format_args['station_id'] = record['natl_station_code']
-        #     format_args['station_name'] = record['name']
-        #     # if record['municipality'] is not None and len(record['municipality']) > 0:
-        #     #     format_args['name'] += ' - ' + record['municipality']
-        #     format_args['station_altitude'] = record['altitude']
-        #     format_args['station_lon'] = record['lon']
-        #     format_args['station_lat'] = record['lat']
-        #
-        #     payload = self.post_thing_templ_str.format(**format_args)
-        #     # print payload
-        # else:
-        #     if self.sos_request == 'insert-observation':
-        #         # with open('/etc/hosts') as f:
-        #         #     print f.read()
 
         return payload
