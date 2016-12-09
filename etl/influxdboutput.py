bb7a543f18d2ad2d2fe6a36bcfce3d6b22396c53
Just van den Broecke <just@justobjects.nl>
2016-12-10 00:28:27 +0100

    #29 first working version generic InfluxDB HTTP Output writer

--- etl/influxdboutput.py
+++ etl/influxdboutput.py
@@ -19,60 +19,140 @@ class InfluxDbOutput(HttpOutput):
     Output via InfluxDB protocol over plain HTTP.
 
     consumes=FORMAT.record_array
+
+    Config example:
+    # Write records to remote InfluxDB server
+    [output_influxdb_write]
+    class = influxdboutput.InfluxDbOutput
+    input_format = record_array
+    method = POST
+    list_fanout = False
+    content_type = application/x-www-form-urlencoded
+    host = test.smartemission.nl
+    port = 8086
+    database = smartemission
+    measurement = joseraw
+    tags_map = {{'station': 'device_id', 'component': 'name' }}
+    fields_map = {{'value': 'value'}}
+    time_attr = time
+    user = theuser
+    password = thepass
+
     """
 
-    @Config(ptype=str, default='joseraw', required=False)
+    @Config(ptype=str, required=True)
+    def database(self):
+        """
+        The "database" is a db-like entity in an InfluxDB database
+
+        Required: True
+
+        Default: N.A.
+        """
+        pass
+
+    @Config(ptype=str, required=True)
     def measurement(self):
         """
-        .
+        The "measurement" is a table-like entity in an InfluxDB database
 
-        Required: False
+        Required: True
 
-        Default: joseraw
+        Default: N.A.
         """
         pass
 
-    @Config(ptype=list, default=['station', 'component'], required=False)
-    def tag_keys(self):
+    @Config(ptype=dict, required=True)
+    def tags_map(self):
         """
-        The tag keys.
+        The tag keys, sort of free-to-choose columns with their mappings to attributes in incoming records.
+        Example: {'station': 'device_id', 'component': 'name' }
+        Required: True
 
-        Required: False
-
-        Default: ['value']
+        Default: N.A.
         """
         pass
 
-    @Config(ptype=list, default=['value'], required=False)
-    def field_keys(self):
+    @Config(ptype=dict, required=True)
+    def fields_map(self):
         """
-        The value fields.
+        The actual value fields with their mapping to record attributes.
 
-        Required: False
+        Required: True
 
-        Default: ['value']
+        Default: N.A.
+        """
+        pass
+
+    @Config(ptype=str, required=True)
+    def time_attr(self):
+        """
+        The time record attribute to use for InfluxDB time field.
+
+        Required: True
+
+        Default: N.A.
         """
         pass
 
     def __init__(self, configdict, section):
         HttpOutput.__init__(self, configdict, section, consumes=FORMAT.record_array)
 
-        # Construct the template line for each data frame
-        self.template_data_line = self.measurement + ',station=%d,component=%s value=%d %s'
+        # Construct write path
+        self.path = '/write?db=%s' % self.database
+
+        # Construct the template line to be used for each data frame
+        # e.g. 'joseraw,station=%s,component=%s value=%s %s'
+        self.tags = self.tags_map.keys()
+        self.fields = self.fields_map.keys()
+        self.tags_template = ''
+        for tag in self.tags:
+            self.tags_template += ',' + tag + '=%s'
+
+        self.fields_template = ' '
+        for field in self.fields:
+            self.fields_template += field + '=%s '
+
+        # will expand to e.g. joseraw,station=19,component=no2raw value=12345 1434055562000000000
+        # tags, fields and timestamp will be filled when creating payload
+        self.template_data_line = self.measurement + '%s%s%s\n'
 
     def create_payload(self, packet):
-        # Create the POST payload as for example: to host http://data.smartemission.nl:8086/write?db=smartemraw
+        # Create the POST body (payload) as for example: to host http://data.smartemission.nl:8086/write?db=smartemraw
         # joseraw,station=19,component=no2raw value=12345 1434055562000000000
         # joseraw,station=23,component=temperature value=18 1434055562000000000
         # joseraw,station=19,component=o3raw value=12345 1434055562000000000'
 
-        record = packet.data
+        # Handle both record_array and single record
+        records = packet.data
+        if type(packet.data) is not list:
+            # packet data is single record
+            records = [packet.data]
 
-        # Make Timestamp in Unixtime nanosecs, e.g. 1434055562000000000
-        t = record['time']
-        tstamp = int(time.mktime(t.timetuple())) * 1000 * 1000
+        payload = ''
+        nanos = int(1000 * 1000 * 1000) # constant for time conversion
 
-        # Create InfluxDB line protocol string
-        payload = self.template_data_line % (record['device_id'], record['name'], record['value'], tstamp)
+        for record in records:
+
+            # Make Timestamp in Unixtime nanosecs, e.g. 1434055562000000000
+            # delete: DELETE FROM joseraw WHERE time > '1970-01-01'
+            tstamp_nanos = int(time.mktime(record[self.time_attr].timetuple()) * nanos)
+
+            # Create tags substring
+            tag_values = []
+            for tag in self.tags:
+                tag_values.append(str(record[self.tags_map[tag]]))
+            tag_values = tuple(tag_values)
+            tags = self.tags_template % tag_values
+
+            # Create fields substring
+            field_values = []
+            for field in self.fields:
+                field_values.append(str(record[self.fields_map[field]]))
+            field_values = tuple(field_values)
+            fields = self.fields_template % field_values
+
+            # Assemble InfluxDB line protocol string, each measurement on a new line
+            payload += self.template_data_line % (tags, fields, tstamp_nanos)
 
         return payload
