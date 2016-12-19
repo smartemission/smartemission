9b06e27e8b8aff4c14cde964d1d3f210ccd91fc8
Pieter Marsman <pietermarsman@gmail.com>
2016-12-19 14:15:48 +0100

    Selecting jose data from influxdb

--- etl/calibration/src/python/load_data.py
+++ etl/calibration/src/python/load_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from influxdb import DataFrameClient
 
 
 def get_rivm_and_jose_data_from_prepared_file(path):
@@ -18,5 +19,17 @@ def get_rivm_data():
     pass
 
 
-def get_jose_data():
-    pass
+def get_jose_data(host, port, user, password, dbname, series_name, limit=None):
+    query = "SELECT * FROM %s" % series_name
+    if limit is not None:
+        query += " LIMIT %d" % limit
+
+    client = DataFrameClient(host, port, user, password, dbname)
+    client.switch_database(dbname)
+    df = client.query(query)[series_name]
+    return df
+
+
+# if __name__ == '__main__':
+#     print(get_jose_data("localhost", 8086, "pieter_reader", "pieter_pass",
+#                   "smartemission", "joseextract", 10))
