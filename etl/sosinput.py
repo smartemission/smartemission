761d7053c4d85b095a19699bfb8cb1bd7186718f
Pieter Marsman <pietermarsman@gmail.com>
2016-12-23 17:03:29 +0100

    Cleaning up sosinput

--- etl/sosinput.py
+++ etl/sosinput.py
@@ -1,21 +1,19 @@
 import json
+import re
+from datetime import datetime
 from stetl.component import Config
 from stetl.inputs.dbinput import PostgresDbInput
 from stetl.inputs.httpinput import HttpInput
 from stetl.util import Util
 
-from datetime import datetime, timedelta
-
-import re
-
 log = Util.get_log("SosInput")
+
 
 # http://inspire.rivm.nl/sos/eaq/api/v1/features/?service=RIVM_inspire&locale=en
 # http://inspire.rivm.nl/sos/eaq/api/v1/categories/?service=RIVM_inspire&locale=en&feature=303
 # http://inspire.rivm.nl/sos/eaq/api/v1/phenomena/?service=RIVM_inspire&locale=en&feature=303&category=130
 # http://inspire.rivm.nl/sos/eaq/api/v1/procedures/?service=RIVM_inspire&locale=en&feature=303&category=130&phenomenon=130
 # http://inspire.rivm.nl/sos/eaq/api/v1/timeseries/303/getData?timespan=2016-12-20T22%3A00%3A00%2B01%3A00%2F2016-12-22T01%3A59%3A59%2B01%3A00&
-
 
 class SosInput(HttpInput):
     """
@@ -71,6 +69,7 @@ class SosInput(HttpInput):
     def __init__(self, configdict, section):
         HttpInput.__init__(self, configdict, section)
         self.base_url = self.url
+        self.url = None
 
         self.feature_idx = 0
         self.next_timestamp = -1
@@ -78,9 +77,10 @@ class SosInput(HttpInput):
 
     def init(self):
         for feature in self.features:
-            timeseries_url = self.base_url + self.timeseries_path % feature
-            log.info("Searching timeseries info for %s" % timeseries_url)
-            json_str = self.read_from_url(timeseries_url)
+            time_series_url = self.base_url + self.timeseries_path % feature
+            log.info("Searching info for %s" % time_series_url)
+
+            json_str = self.read_from_url(time_series_url)
             json_obj = self.parse_json_str(json_str)
             if json_obj is not None:
                 self.feature_info[feature] = json_obj
@@ -89,18 +89,19 @@ class SosInput(HttpInput):
         self.next_entry()
 
     def after_invoke(self, packet):
-        if not self.more_features_available() and not self.more_data_available():
+        if not (self.more_features_available() or self.more_data_available()):
             log.info("No more features and data available")
             packet.set_end_of_stream()
+
         if len(packet.data) is 0:
-            log.warn("No data for %s" % self.current_date().isoformat())
+            log.info("No data for %s" % self.current_date().isoformat())
 
     def format_data(self, data):
         json_obj = self.parse_json_str(data)["values"]
-        log.info("Received json object of length %d" % len(json_obj))
+        log.info("Formatting %d elements of json response" % len(json_obj))
 
         info = self.current_feature_info()
-        id = info['id']
+        feature_id = info['id']
         label = info['label']
         station_name = info['station']['properties']['label']
         # feature_name = info['parameters']['feature']['label']
@@ -108,15 +109,14 @@ class SosInput(HttpInput):
 
         lat, lon, alt = info['station']['geometry']['coordinates']
         for elem in json_obj:
-            elem['id'] = id
+            elem['id'] = feature_id
             elem['label'] = SosInput.save_name(label)
             elem['station_name'] = SosInput.save_name(station_name)
             elem['phenomenon_name'] = SosInput.save_name(phenomenon_name)
             elem['lat'] = lat
             elem['lon'] = lon
             elem['altitude'] = alt
-            elem['datetime'] = datetime.fromtimestamp(elem['timestamp'] /
-                                                       1000)
+            elem['datetime'] = datetime.fromtimestamp(elem['timestamp'] / 1000)
 
         return json_obj
 
@@ -124,23 +124,31 @@ class SosInput(HttpInput):
         if self.next_timestamp < 0:
             self.set_first_timestamp()
         else:
-            self.next_timestamp += self.request_hours * 60*60*1000
+            self.next_timestamp += self.request_hours * 60 * 60 * 1000
         if not self.more_data_available() and self.more_features_available():
             self.feature_idx += 1
             self.next_timestamp = -1
-            log.info("Switching to next feature %s because assuming nothing "
-                     "is known about the future" % self.current_feature())
-        self.url = self.current_url()
+            log.info("Switching to next feature %s" % self.current_feature())
 
-        log.info("New url: %s" % self.url)
+        self.url = self.current_url()
+        log.info("Next entry is %s" % self.url)
 
     def more_features_available(self):
-        return self.feature_idx < len(self.features)-1
+        """
+        :return: If there are more features after the current one
+        """
+        return self.feature_idx < len(self.features) - 1
 
     def more_data_available(self):
+        """
+        :return: If there is more data for the current feature
+        """
         return self.current_date() < datetime.now()
 
     def current_url(self):
+        """
+        :return: The url for the current feature and date
+        """
         params = (self.current_feature(), self.current_date().isoformat(),
                   self.request_hours)
         return self.base_url + self.get_data_path % params
@@ -167,7 +175,6 @@ class SosInput(HttpInput):
             json_obj = json.loads(raw_str)
         except Exception, e:
             log.error('Cannot parse JSON from %s, err= %s' % (raw_str, str(e)))
-            raise e
 
         return json_obj
 
@@ -177,7 +184,6 @@ class SosInput(HttpInput):
 
 
 class RIVMSosInput(SosInput, PostgresDbInput):
-
     @Config(ptype=str, required=True)
     def progress_query(self):
         """
@@ -185,6 +191,7 @@ class RIVMSosInput(SosInput, PostgresDbInput):
 
         Required: True
         """
+
     def __init__(self, configdict, section):
         SosInput.__init__(self, configdict, section)
         PostgresDbInput.__init__(self, configdict, section)
@@ -193,20 +200,25 @@ class RIVMSosInput(SosInput, PostgresDbInput):
     def init(self):
         SosInput.init(self)
         PostgresDbInput.init(self)
-        log.info("Querying progress database with %s" % self.progress_query)
 
-        progres_list = self.do_query(self.progress_query)
-        for progres_row in progres_list:
-            name = progres_row["name"]
-            timestamp = progres_row["timestamp"]
-            self.progress[progres_row['name']] = progres_row['timestamp']
+        progress_list = self.do_query(self.progress_query)
+        for progress_row in progress_list:
+            name = progress_row["name"]
+            timestamp = progress_row["timestamp"]
+            self.progress[name] = timestamp
 
     def set_first_timestamp(self):
         info = self.current_feature_info()
+
+        # if there is already progress for label
         if info["label"] in self.progress:
             self.next_timestamp = self.progress[info["label"]]
-            log.info("Read progress table for %s" % self.current_feature())
+            log.info("Using progress table to initialize first timestamp "
+                     "for %s" % self.current_feature())
+        # if there is no progress for label
         else:
             self.next_timestamp = info["firstValue"]["timestamp"]
-            log.info("Read RIVM firstValue timestamp for %s" % self.current_feature())
+            log.info("Using RIVM SOS to initialize first timestamp "
+                     "for %s" % self.current_feature())
+
         log.info("Next timestamp is %s" % self.current_date().isoformat())
