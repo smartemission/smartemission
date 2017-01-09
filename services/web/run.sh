602dad4b29500d4c3161024d778e2294d8dd0067
Just van den Broecke <just@justobjects.nl>
2017-01-09 22:07:15 +0100

    add influx db as linked webservice

--- services/web/run.sh
+++ services/web/run.sh
@@ -14,13 +14,14 @@ IMAGE="geonovum/apache2"
 PG_HOST="postgis"
 GS_HOST="geoserver"
 SOS52N_HOST="sos52n"
+INFLUXDB_HOST="influxdb"
 
 # Somehow needed for mounts
 chmod 777 ${LOG} ${BACKUP}
 
 VOL_MAP="-v ${CONFIG}/admin:/etc/apache2/admin -v ${CONFIG}/phppgadmin:/etc/phppgadmin -v ${CONFIG}/sites-enabled:/etc/apache2/sites-enabled -v ${GIT}:${GIT} -v ${LOG}/apache2:/var/log/apache2 -v ${LOG}:/smartemlogs -v ${BACKUP}:/smartembackups"
 PORT_MAP="-p 80:80"
-LINK_MAP="--link ${PG_HOST}:${PG_HOST} --link ${GS_HOST}:${GS_HOST} --link ${SOS52N_HOST}:${SOS52N_HOST}"
+LINK_MAP="--link ${PG_HOST}:${PG_HOST} --link ${GS_HOST}:${GS_HOST} --link ${SOS52N_HOST}:${SOS52N_HOST} --link ${INFLUXDB_HOST}:${INFLUXDB_HOST}"
 
 # Stop and remove possibly old containers
 sudo docker stop ${NAME} > /dev/null 2>&1
