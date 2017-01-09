98b4b2545db9ccd3549e6c6bbf96f66932b57e88
Pieter Marsman <pietermarsman@gmail.com>
2017-01-09 17:27:26 +0100

    Visualizing residuals in a scatter plot

--- etl/calibration.py
+++ etl/calibration.py
@@ -1,11 +1,20 @@
 from stetl.component import Config
 from stetl.filter import Filter
+from stetl.output import Output
 from stetl.packet import FORMAT
 from stetl.util import Util
 
 import pandas as pd
+import os
+import matplotlib
+
+matplotlib.use('Agg')
+import seaborn as sns
 from numpy import nan
+from sklearn.metrics import explained_variance_score
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
@@ -181,6 +190,7 @@ class Calibrator(Filter):
 
         # Split into label and data
         x, y = Calibrator.split_data_label(df, self.target)
+        x, x_test, y, y_test = train_test_split(x, y, test_size=.9)
 
         log.info("Starting randomized cross validated search to find best "
                  "parameters. Running %d iterations with %d cross "
@@ -198,6 +208,8 @@ class Calibrator(Filter):
         for gs_keys in ['cv_results_', 'best_estimator_', 'best_score_',
                         'best_params_', 'best_index_', 'scorer_', 'n_splits_']:
             result_out[gs_keys] = getattr(gs, gs_keys)
+        result_out['out_of_bag'] = (x_test, y_test)
+        result_out['target'] = self.target
 
         packet.data = result_out
         log.info("Returning result of %d length, with keys %s." % (
@@ -234,3 +246,179 @@ class Calibrator(Filter):
             val = elem * alpha + val * (1.0 - alpha)
             new_x.append(val)
         return pd.Series(new_x)
+
+
+class Visualization(Output):
+    @Config(ptype=str, required=True)
+    def file_path(self):
+        """
+        The path where to save the visualization images. Should contain a %s that is replaced by the image name.
+
+        Required: True
+        """
+
+    def __init__(self, configdict, section):
+        Output.__init__(self, configdict, section, consumes=FORMAT.record)
+        self.model = None
+        self.oob = None
+        self.x = None
+        self.y = None
+        self.df = None
+        self.cv_results_ = None
+        self.err = False
+
+    def write(self, packet):
+        record_in = packet.data
+
+        self.model = record_in['best_estimator_']
+
+        self.oob = record_in['out_of_bag']
+        self.x = self.oob[0]
+        self.y = self.oob[1]
+        self.df = self.x.copy()
+        self.df['Target'] = self.y
+
+        self.cv_results_ = record_in['cv_results_']
+
+        self.target = record_in['target']
+
+        dirname = os.path.dirname(self.file_path)
+        log.info("Creating dir %s" % dirname)
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+        self.visualize()
+
+        return packet
+
+    def visualize(self):
+        pass
+
+
+class ModelVisualization(Visualization):
+    def __init__(self, configdict, section):
+        Visualization.__init__(self, configdict, section)
+        self.pred = None
+
+    def visualize(self):
+        self.pred = self.model.predict(self.x)
+        self.df['Prediction'] = self.pred
+
+        self.visualize_error()
+        self.visualize_input_output_relation()
+
+    def visualize_error(self):
+        # Create title (use both explained variance and rmse to hava a scale
+        # relative and absolute measurement of performance)
+        expl_var = explained_variance_score(self.y, self.pred)
+        rmse = mean_squared_error(self.y, self.pred) ** .5
+        title = 'RMSE=%.1f ug/m3, Explained var=%.0f%%' % (
+            rmse, expl_var * 100)
+
+        # Plot using seaborn
+        g = sns.regplot('Prediction', 'Target', self.df)
+        g.set_title(title)
+        g.set_aspect('equal', 'datalim')
+
+        # Save
+        file_path = self.file_path % 'scatter.png'
+        sns.plt.savefig(file_path)
+        log.info("Saved scatterplot to %s" % file_path)
+
+    def visualize_input_output_relation(self):
+        pass
+
+
+class SearchVisualization(Visualization):
+    def write(self, packet):
+        pass
+
+    def visualize(self):
+        self.visualize_search()
+
+    def visualize_search(self):
+        pass
+
+# def visualize_residuals(pred, perf, limits, name, path):
+#     pred['residual'] = pred['target'] - pred['prediction']
+#
+#     title = '%s\nRMSE=%.2f, Explained var=%.2f%%' % (
+#     name, perf['rmse'], perf['expl_var'])
+#     p = ggplot(pred, aes('residual')) + geom_histogram(bins=500) + xlab(
+#         'Residual (ug\m3)') + ylab('Count') + xlim(limits[0],
+#                                                    limits[1]) + ggtitle(
+#         title)
+#     p.save(path)
+#
+# def visualize_timeseries(df, start, end, limits, name, path):
+#     start = to_datetime(start)
+#     end = to_datetime(end)
+#     col_num = 'p.unit.serial.number'
+#
+#     df['secs'] = to_datetime(df['secs'], format='%Y%m%d%H%M%S')
+#     df = melt(df, [col_num, 'secs'], ['prediction', 'target'], 'Type')
+#     df = df[(df['secs'] >= start) & (df['secs'] <= end)]
+#     df[col_num] = df[col_num].astype(int).astype(str)
+#     df['Location'] = df['Type'].str.cat(df['p.unit.serial.number'])
+#
+#     p = ggplot(df, aes(x='secs', y='value', color='Type')) + geom_line(
+#         size=2) + ylab('RIVM measurement / Prediction (ug/m3)') + ylim(
+#         limits[0], limits[1]) + ggtitle(name)
+#     if len(df[col_num].unique()) > 1:
+#         p = p + facet_grid(col_num)
+#     p.save(path)
+#
+# def visualize_occurance(pred, col, path):
+#     p = ggplot(pred, aes(col)) + geom_histogram(bins=100) + xlab(
+#         col) + ylab('Count') + ggtitle('Occurance of %s values' % col)
+#     p.save(path)
+#
+# def plot_ann_effect(pipeline, filter, df, col, val, limits, name, path):
+#     n_times = 100
+#     n_val = val.shape[0]
+#     df = df.drop(['prediction', 'target'], axis=1)
+#     df = filter.transform(df)
+#     df = df.sample(n_times)
+#     df = concat([df] * n_val)
+#     df[col] = repeat(val, n_times)
+#     df['prediction'] = pipeline.predict(df)
+#     df['id'] = tile(arange(0, n_times), n_val)
+#
+#     p = ggplot(df, aes(x=col, y='prediction', group='id')) + geom_line(
+#         alpha=.5) + xlab('Jose measurement of %s' % col) + ylab(
+#         'Prediction for %s' % name) + ylim(limits[0], limits[1]) + ggtitle(
+#         'Effect of %s on predictions for %s' % (col, name))
+#     p.save(path)
+#
+# if __name__ == '__main__':
+#     col = sys.argv[1]
+#     t = float(sys.argv[2])
+#
+#     f_param_optim = save_path('parameter_optimization', col, 'csv', t)
+#     f_predictions = save_path('predictions', col, 'csv', t)
+#     f_performance = save_path('performances', col, 'json', t)
+#     f_model = save_path('model', col, 'pkl', t)
+#     f_filter = save_path('filter', col, 'pkl', t)
+#
+#     f_scatter = save_path('scatter', col, 'png', t)
+#     f_residual = save_path('residual', col, 'png', t)
+#     f_timeseries = save_path('timeseries', col, 'png', t)
+#
+#     pred = load_predictions(f_predictions)
+#     perf = load_performances(f_performance)
+#     model = load_model(f_model)
+#     filter = load_filter(f_filter)
+#
+#     visualize_scatter(pred.copy(), perf, col, ax_limits[col], f_scatter)
+#     visualize_residuals(pred.copy(), perf, res_limits[col], col,
+#                         f_residual)
+#     visualize_timeseries(pred.copy(), "20160515", "20160526",
+#                          ax_limits[col], col, f_timeseries)
+#
+#     for name in pred.columns.values:
+#         f_occurance = save_path('histogram_%s' % name, col, 'png', t)
+#         visualize_occurance(pred.copy(), name, f_occurance)
+#
+#     for k, v in effect_plots.iteritems():
+#         f_effect = save_path('ann_effect_%s' % k, col, 'png', t)
+#         plot_ann_effect(model, filter, pred.copy(), k, v, ax_limits[col],
+#                         col, f_effect)
--- etl/calibration_parameters.py
+++ etl/calibration_parameters.py
@@ -59,26 +59,26 @@ param_grid = {'mlp__hidden_layer_sizes': randint(2, 150),
               # 'filter__alpha': ExpDistribution(uniform(-3, 2)),
               'mlp__early_stopping': [True]}
 
-CO_best = {'mlp__activation': 'relu', 'mlp__alpha': 1.899210794532138e-06,
-           'mlp__hidden_layer_sizes': 112,
-           'mlp__learning_rate_init': 0.07908998568339845,
-           'mlp__solver': 'lbfgs', 'mlp__learning_rate': 'constant',
-           'mlp__max_iter': 200}
-
-O3_best = {'mlp__activation': 'logistic', 'mlp__alpha': 1.4863204889431821e-05,
-           'mlp__hidden_layer_sizes': 141,
-           'mlp__learning_rate_init': 0.004551945724274247,
-           'mlp__solver': 'lbfgs', 'mlp__learning_rate': 'constant',
-           'mlp__max_iter': 200}
-
-NO2_best = {'mlp__activation': 'tanh', 'mlp__alpha': 1.4312431413452899e-05,
-            'mlp__hidden_layer_sizes': 71,
-            'mlp__learning_rate_init': 4.263969225750196e-05,
-            'mlp__solver': 'lbfgs', 'mlp__learning_rate': 'constant',
-            'mlp__max_iter': 200}
-
-test_grid = param_grid.copy()
-test_grid['mlp__hidden_layer_sizes'] = randint(2, 10)
-
-best_params = {'CO_Waarden': CO_best, 'O3_Waarden': O3_best,
-               'NO2_Waarden': NO2_best}
+# CO_best = {'mlp__activation': 'relu', 'mlp__alpha': 1.899210794532138e-06,
+#            'mlp__hidden_layer_sizes': 112,
+#            'mlp__learning_rate_init': 0.07908998568339845,
+#            'mlp__solver': 'lbfgs', 'mlp__learning_rate': 'constant',
+#            'mlp__max_iter': 200}
+#
+# O3_best = {'mlp__activation': 'logistic', 'mlp__alpha': 1.4863204889431821e-05,
+#            'mlp__hidden_layer_sizes': 141,
+#            'mlp__learning_rate_init': 0.004551945724274247,
+#            'mlp__solver': 'lbfgs', 'mlp__learning_rate': 'constant',
+#            'mlp__max_iter': 200}
+#
+# NO2_best = {'mlp__activation': 'tanh', 'mlp__alpha': 1.4312431413452899e-05,
+#             'mlp__hidden_layer_sizes': 71,
+#             'mlp__learning_rate_init': 4.263969225750196e-05,
+#             'mlp__solver': 'lbfgs', 'mlp__learning_rate': 'constant',
+#             'mlp__max_iter': 200}
+#
+# test_grid = param_grid.copy()
+# test_grid['mlp__hidden_layer_sizes'] = randint(2, 10)
+#
+# best_params = {'CO_Waarden': CO_best, 'O3_Waarden': O3_best,
+#                'NO2_Waarden': NO2_best}
