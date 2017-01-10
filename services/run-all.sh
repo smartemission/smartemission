d7761b7f815fba44a93e03519acc36197155202b
Just van den Broecke <just@justobjects.nl>
2017-01-10 17:07:56 +0100

    #30 Grafana now available on platform

--- services/run-all.sh
+++ services/run-all.sh
@@ -4,7 +4,7 @@
 #
 script_dir=${0%/*}
 
-CONTAINERS="postgis geoserver sos52n web influxdb grafana"
+CONTAINERS="postgis influxdb grafana geoserver sos52n web"
 for CONTAINER in ${CONTAINERS}
 do
   echo "starting ${CONTAINER}"
