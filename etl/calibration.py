c5629347d3016768748a1fa6a691348c27d71ca5
Pieter Marsman <pietermarsman@gmail.com>
2017-01-15 22:51:26 +0100

    Added class for reading calibration models

--- etl/calibration.py
+++ etl/calibration.py
@@ -515,6 +515,56 @@ class CalibrationModelOutput(PostgresInsertOutput):
         return packet
 
 
+class CalibrationModelInput(PostgresDbInput):
+    """
+    Get unpickled calibration model from the database
+    """
+
+    @Config(ptype=dict, default=dict(), required=False)
+    def sensor_model_names(self):
+        """
+        The name of the sensor models in the database. Needed for linking
+        the right model to the right gas.
+
+        Default: dict()
+
+        Required: False
+        """
+
+    def __init__(self, configdict, section):
+        PostgresDbInput.__init__(self, configdict, section)
+        self.models = None
+
+    def init(self):
+        PostgresDbInput.init(self)
+
+        def get_model(name):
+            query = self.query % name
+            log.info('Getting calibration model with query: %s' % query)
+            ret = self.raw_query(query)
+            if len(ret) > 0:
+                return pickle.loads(str(ret[0][0]))
+            else:
+                log.warn("No model found for %s" % name)
+                return None
+
+        if self.query is not None and len(
+                self.sensor_model_names) > 0:
+            log.info('Getting calibration models from database')
+            self.models = {k: get_model(v) for k, v in
+                           self.sensor_model_names.iteritems()}
+        else:
+            log.info(
+                'No query for fetching calibration models given or no '
+                'mapping for calibration models to gas components given.')
+
+    def invoke(self, packet):
+        if packet.data is not None:
+            packet.data['models'] = self.models
+
+        return packet
+
+
 # done time, station, target, prediction, error in different data frame
 # done plot decorator
 # done split viz into multiple classes
