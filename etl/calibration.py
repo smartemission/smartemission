0e14ab77518d473f89557cf6e6c477acc9b53336
Pieter Marsman <pietermarsman@gmail.com>
2017-01-25 13:54:05 +0100

    #39 no filtering such that calibration model is stateless. When having a (time) state each process should have its own instance of the model.

--- etl/calibration.py
+++ etl/calibration.py
@@ -104,16 +104,6 @@ class Calibrator(Filter):
         Required: True
         """
 
-    @Config(ptype=float, default=1, required=False)
-    def filter_alpha(self):
-        """
-        Control for low-pass filter, higher alpha is more emphasis on new data
-
-        Default: 0.01
-
-        Required: False
-        """
-
     @Config(ptype=list, required=True)
     def targets(self):
         """
@@ -190,10 +180,6 @@ class Calibrator(Filter):
         df = df.drop(self.other_targets, axis=1)
         df = df.reset_index() # we want a clean data frame
         log.info('Created data frame with shape (%d, %d)' % df.shape)
-
-        # Pre-processing: filter data
-        filter_col = ['s_coresistance', 's_no2resistance', 's_o3resistance']
-        df = Calibrator.filter_data(df, filter_col, self.filter_alpha)
 
         # Sample to prevent over fitting
         df = df.sample(frac=1.0 / float(self.inverse_sample_fraction))
