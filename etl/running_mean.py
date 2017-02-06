b6f32c7c76e00a3e1d064d5556aa713470ad1878
Pieter Marsman <pietermarsman@gmail.com>
2017-02-06 23:10:24 +0100

    #48 creating statefull RunningMean class that can also be applied on a DataFrame

---
+++ etl/running_mean.py
@@ -0,0 +1,82 @@
+class RunningMean(object):
+
+    def __init__(self, new_obs_weight, state_init="first"):
+        """
+        Apply running mean on streaming data. New running mean is computed
+        like:
+        new_state = old_state * (1 - new_obs_weight) + obs * new_obs_weight
+
+        :param new_obs_weight: weight given to new obs
+        :param state_init: way to initialize the state. Can be numeric or
+            the string "first" in which case the first obs is used.
+        """
+        self.new_obs_weight = new_obs_weight
+        self.state = state_init
+
+    def observe(self, obs):
+        """
+        Update running mean with new observation
+
+        :param obs: observation
+        :return: the new running mean
+        """
+        # convert to float and test if obs can be interpreted as numeric at
+        # the same time
+        obs = float(obs)
+
+        if self.state == "first":
+            self.state = obs
+
+        self.state = RunningMean.running_mean(self.state, obs,
+                                              self.new_obs_weight)
+        return self.state
+
+    @staticmethod
+    def running_mean(state, obs, weight):
+        """
+        New state of a running mean given observation and weight
+
+        :param state: old state
+        :param obs: new observation
+        :param weight: weight of observation
+        :return: new state
+        """
+        return state * (1.0 - weight) + obs * weight
+
+    @staticmethod
+    def series_running_mean(series, new_obs_weight, init = "first"):
+        """
+        Apply running mean on a the numeric pandas Series.
+
+        In comparison to the pandas .rolling functions this has a state
+        while the pandas function have a window.
+
+        :param series: a pandas Series
+        :param new_obs_weight: weight given to new obs
+        :return: filtered series
+        """
+        state = init
+        if state == "first":
+            state = series[0]
+
+        states = []
+        for i in series:
+            state = RunningMean.running_mean(state, i, new_obs_weight)
+            states.append(state)
+        series[:] = states
+
+        return series
+
+if __name__ == "__main__":
+    from pandas import Series
+    a = Series([1, 10, 100, 1000])
+    b = RunningMean.series_running_mean(a, .5)
+    print(b)
+    # should be: Series([1, 5.5, 52.75, 526.375])
+    state = RunningMean(.5)
+    states = []
+    for i in a:
+        state.observe(i)
+        states.append(i)
+    print(states)
+    # should be [1, 5.5, 52.75, 526.375]
