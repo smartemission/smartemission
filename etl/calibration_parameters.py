745dada4fa66c9103eb25efe77cc3946ca568210
Pieter Marsman <pietermarsman@gmail.com>
2017-07-22 15:42:44 +0200

    New calibration parameters, that learn deeper model

--- etl/calibration_parameters.py
+++ etl/calibration_parameters.py
@@ -49,36 +49,9 @@ class FixedLengthTupleDistribution:
                       self.distributions])
 
 
-param_grid = {'mlp__hidden_layer_sizes': randint(2, 150),
-              'mlp__activation': ['logistic', 'tanh', 'relu'],
-              'mlp__solver': ['lbfgs'],
-              'mlp__alpha': ExpDistribution(uniform(-6, 5)),
-              'mlp__learning_rate': ['constant'], 'mlp__max_iter': [200],
-              'mlp__learning_rate_init': ExpDistribution(uniform(-6, 5)),
-              # no2 results analysis shows good range between 0.1 and 0.001
-              # 'filter__alpha': ExpDistribution(uniform(-3, 2)),
-              'mlp__early_stopping': [True]}
-
-# CO_best = {'mlp__activation': 'relu', 'mlp__alpha': 1.899210794532138e-06,
-#            'mlp__hidden_layer_sizes': 112,
-#            'mlp__learning_rate_init': 0.07908998568339845,
-#            'mlp__solver': 'lbfgs', 'mlp__learning_rate': 'constant',
-#            'mlp__max_iter': 200}
-#
-# O3_best = {'mlp__activation': 'logistic', 'mlp__alpha': 1.4863204889431821e-05,
-#            'mlp__hidden_layer_sizes': 141,
-#            'mlp__learning_rate_init': 0.004551945724274247,
-#            'mlp__solver': 'lbfgs', 'mlp__learning_rate': 'constant',
-#            'mlp__max_iter': 200}
-#
-# NO2_best = {'mlp__activation': 'tanh', 'mlp__alpha': 1.4312431413452899e-05,
-#             'mlp__hidden_layer_sizes': 71,
-#             'mlp__learning_rate_init': 4.263969225750196e-05,
-#             'mlp__solver': 'lbfgs', 'mlp__learning_rate': 'constant',
-#             'mlp__max_iter': 200}
-#
-# test_grid = param_grid.copy()
-# test_grid['mlp__hidden_layer_sizes'] = randint(2, 10)
-#
-# best_params = {'CO_Waarden': CO_best, 'O3_Waarden': O3_best,
-#                'NO2_Waarden': NO2_best}
+param_grid = {'mlp__hidden_layer_sizes': [(30, 30, 30), (40, 40, 40), (50, 50, 50), (60, 60, 60), (80, 80, 80)],
+              'mlp__activation': ['logistic'],
+              'mlp__solver': ['adam'],
+              'mlp__alpha': [0.02, 0.01, 0.005, 0.002, 0.001],
+              'mlp__learning_rate_init': [0.01, 0.005, 0.001]
+              }
