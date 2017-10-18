a8e9d076e227006ed99ea32e2126d3cbfe3fddca
Just van den Broecke <just@justobjects.nl>
2017-10-18 15:07:53 +0200

    #60 make only active SE stations visible

--- services/web/site/heron/config/Map.js
+++ services/web/site/heron/config/Map.js
@@ -400,7 +400,7 @@ Heron.options.map.layers = [
         Heron.scratch.urls.SMARTEM_OWS,
         {layers: "smartem:stations", format: "image/png", transparent: true},
         {
-            isBaseLayer: false, singleTile: true, visibility: true, alpha: true,
+            isBaseLayer: false, singleTile: true, visibility: false, alpha: true,
             featureInfoFormat: "application/vnd.ogc.gml", transitionEffect: 'resize',
             metadata: {
                 wfs: {
@@ -445,7 +445,7 @@ Heron.options.map.layers = [
         Heron.scratch.urls.SMARTEM_OWS,
         {layers: "smartem:stations", format: "image/png", transparent: true, styles: "stations-inactive"},
         {
-            isBaseLayer: false, singleTile: true, visibility: true, alpha: true,
+            isBaseLayer: false, singleTile: true, visibility: false, alpha: true,
             featureInfoFormat: "application/vnd.ogc.gml", transitionEffect: 'resize',
             metadata: {
                 wfs: {
