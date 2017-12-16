357fc8f3e2b7a5ed94a9656c6065288f815c83c1
Just van den Broecke <just@justobjects.nl>
2017-12-16 16:58:28 +0100

    #63 monitoring: add Prometheus

---
+++ services/prometheus/README.md
@@ -0,0 +1,21 @@
+# Prometheus
+
+Monitoring, using Prometheus 2.0+.
+
+## Info
+
+Tutorial:
+https://www.digitalocean.com/community/tutorials/how-to-install-prometheus-using-docker-on-ubuntu-14-04
+
+Setup Node exporter Ubuntu
+https://www.digitalocean.com/community/tutorials/how-to-use-prometheus-to-monitor-your-ubuntu-14-04-server
+
+Docker Compose full example
+https://github.com/vegasbrianc/prometheus/blob/version-2/docker-compose.yml
+
+COnfig full example
+https://github.com/prometheus/prometheus/blob/master/config/testdata/conf.good.yml
+
+A curated list of awesome Prometheus resources, projects and tools.
+https://github.com/roaldnefs/awesome-prometheus
+
---
+++ services/prometheus/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Run the Prometheus server
+#
+# See https://prometheus.io/docs/prometheus/latest/installation/
+SCRIPT_DIR=${0%/*}
+pushd ${SCRIPT_DIR}
+SCRIPT_DIR=$PWD
+popd
+
+source ${SCRIPT_DIR}/../../etl/options/`hostname`.args
+
+LOG_DIR="/var/smartem/log/prometheus"
+DATA_DIR="/var/smartem/data/prometheus"
+CONFIG_DIR="${SCRIPT_DIR}/config"
+NAME="prometheus"
+IMAGE="prom/prometheus:master"
+mkdir -p ${DATA_DIR}
+
+VOL_MAP="-v ${CONFIG_DIR}:/etc/prometheus -v ${DATA_DIR}:/promdata"
+PORT_MAP=""
+LINK_MAP=""
+OPTIONS="--config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/promdata --web.console.libraries=/usr/share/prometheus/console_libraries --web.console.templates=/usr/share/prometheus/consoles --web.external-url=http://${se_host}/prometheus"
+
+# Stop and remove possibly old containers
+sudo docker stop ${NAME} > /dev/null 2>&1
+sudo docker rm ${NAME} > /dev/null 2>&1
+
+# Finally run
+# docker run -p 9090:9090 -v /tmp/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
+
+echo "Run Prometheus with options: ${OPTIONS}"
+docker run -u root --name ${NAME} ${LINK_MAP} ${PORT_MAP} ${VOL_MAP} -t -i ${IMAGE} ${OPTIONS}
