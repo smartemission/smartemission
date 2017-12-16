6260031b87fb2636a881002874848710ef125b7a
Just van den Broecke <just@justobjects.nl>
2017-12-16 16:59:37 +0100

    #63 monitoring: add Prometheus to Apache2

--- services/web/run.sh
+++ services/web/run.sh
@@ -17,8 +17,10 @@ SOS52N_HOST="sos52n"
 INFLUXDB_HOST="influxdb"
 GRAFANA_HOST="grafana"
 STA_GOST_HOST="gost"
+PROMETHEUS_HOST="prometheus"
 
 # In order to have proxy access node_exporter metrics for Prometheus
+# See http://phillbarber.blogspot.nl/2015/02/connect-docker-to-service-on-parent-host.html
 PARENT_HOST=`ip route show | grep docker0 | awk '{print \$9}'`
 
 # Somehow needed for mounts
@@ -26,7 +28,7 @@ sudo chmod 777 ${LOG} ${BACKUP} ${GIT}/etl/calibration
 
 VOL_MAP="-v ${CONFIG}/admin:/etc/apache2/admin -v ${CONFIG}/phppgadmin:/etc/phppgadmin -v ${CONFIG}/sites-enabled:/etc/apache2/sites-enabled -v ${GIT}:${GIT} -v ${LOG}/apache2:/var/log/apache2 -v ${LOG}:/smartemlogs -v ${BACKUP}:/smartembackups"
 PORT_MAP="-p 80:80"
-LINK_MAP="--link ${PG_HOST}:${PG_HOST} --link ${GS_HOST}:${GS_HOST} --link ${SOS52N_HOST}:${SOS52N_HOST} --link ${INFLUXDB_HOST}:${INFLUXDB_HOST} --link ${GRAFANA_HOST}:${GRAFANA_HOST} --link ${STA_GOST_HOST}:${STA_GOST_HOST}"
+LINK_MAP="--link ${PG_HOST}:${PG_HOST} --link ${GS_HOST}:${GS_HOST} --link ${SOS52N_HOST}:${SOS52N_HOST} --link ${INFLUXDB_HOST}:${INFLUXDB_HOST} --link ${GRAFANA_HOST}:${GRAFANA_HOST} --link ${STA_GOST_HOST}:${STA_GOST_HOST} --link ${PROMETHEUS_HOST}:${PROMETHEUS_HOST}"
 # LINK_MAP="--link ${PG_HOST}:${PG_HOST} --link ${STA_GOST_HOST}:${STA_GOST_HOST}"
 
 # Stop and remove possibly old containers
