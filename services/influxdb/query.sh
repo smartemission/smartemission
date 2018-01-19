520a3f967ed0276954c5d6a36ee0d52c211da0e7
Just van den Broecke <just@justobjects.nl>
2018-01-19 23:22:34 +0100

    several cosmetic changes

--- services/influxdb/query.sh
+++ services/influxdb/query.sh
@@ -1,26 +1,45 @@
 #!/bin/bash
 #
 # Fire query on influxdb local instance.
-#
+# usage: ./query.sh host db 'my query'
+# examples:
+# ./query.sh test.smartemission.nl airsenseur "SELECT sampleRawVal FROM JustObjects1"
+# ./query.sh test.smartemission.nl airsenseur "SELECT sampleRawVal FROM JustObjects1 WHERE \"name\" = 'NO2-B43F'"
+# ./query.sh test.smartemission.nl airsenseur "SHOW SERIES FROM Geonovum1"
+# ./query.sh test.smartemission.nl airsenseur 'DROP MEASUREMENT JustObjects1'
 
 pushd ../../etl/db
   . common.sh
 popd
 
+INFLUX_HOST=$1
+INFLUX_DB=$2
+
 # Shorthand func to call InfluxDB REST Query API
 function query() {
-  QUERY="${@}"
-  echo "query: ${QUERY}"
+  QUERY="${1}"
+  echo "host: ${INFLUX_HOST} db=${INFLUX_DB} query: ${QUERY}"
 
   # replace all blanks http://stackoverflow.com/questions/5928156/replace-a-space-with-a-period-in-bash
   # Basically urlencoding.
-  QUERY=${QUERY// /%20}
+  # QUERY=${QUERY// /%20}
 
-  # Invoke curl to local InfluxDB instance to execute the query/command
-  curl \
-   -u ${influx_admin_user}:${influx_admin_password} \
-   -H "Content-Type:text/plain" \
-   -XPOST http://localhost:8086/query?q=${QUERY}
+  # Invoke curl to local InfluxDB instance to execute the query/command   OLD
+#  curl \
+#   -u ${influx_admin_user}:${influx_admin_password} \
+#   -H "Content-Type:text/plain" \
+#   -XPOST http://${INFLUX_HOST}:8086/query?q=${QUERY}
+
+
+	# example curl
+	# curl -G 'http://localhost:8086/query?pretty=true' --data-urlencode "db=mydb" --data-urlencode "q=SELECT value FROM cpu_load_short WHERE region='us-west';SELECT count(value) FROM cpu_load_short WHERE region='us-west'"
+
+   curl  -u ${influx_admin_user}:${influx_admin_password} \
+     -GET "http://${INFLUX_HOST}:8086/query?pretty=true" \
+     --data-urlencode "db=${INFLUX_DB}" \
+     --data-urlencode "q=${QUERY}"
 }
 
-query ${@}
+#
+
+query "$3"
