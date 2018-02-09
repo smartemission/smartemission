a69c3c32204ee71ffd6fb78587d1598c4ca79d85
Just van den Broecke <just@justobjects.nl>
2018-02-09 17:11:36 +0100

    #60 #70 #79 upgrade Gost Docker Image from 0.5 to latest

--- services/gost/run.sh
+++ services/gost/run.sh
@@ -13,15 +13,15 @@ source ../../etl/options/`hostname`.args
 #export gost_server_external_uri
 #docker-compose up
 
-ENV_MAP="-e gost_server_external_uri=${gost_server_external_uri} -e gost_mqtt_host=mosquitto"
-LOG="/var/smartem/log"
+ENV_MAP="-e GOST_SERVER_EXTERNAL_URI=${gost_server_external_uri}"
+LOG="/var/smartem/log/gost"
 NAME="gost"
-IMAGE="geodan/gost:0.5"
+IMAGE="geodan/gost:latest"
 PG_HOST="postgis"
 MOSQUITTO_HOST="mosquitto"
 
 # Define Volume mappings, map local config file
-VOL_MAP="-v ${PWD}/config/config.yaml:/go/bin/gost/config.yaml"
+VOL_MAP="-v ${PWD}/config/config.yaml:/app/config.yaml -v ${LOG}:/var/log"
 
 # If we need to expose 8080 from host
 #PORT_MAP="-p 8080:8080"
@@ -29,9 +29,12 @@ PORT_MAP=""
 #
 LINK_MAP="--link ${PG_HOST}:${PG_HOST} --link ${MOSQUITTO_HOST}:${MOSQUITTO_HOST}"
 
+mkdir -p ${LOG}
 # Stop and remove possibly old containers
 sudo docker stop ${NAME} > /dev/null 2>&1
 sudo docker rm ${NAME} > /dev/null 2>&1
 sudo docker run --name ${NAME} ${LINK_MAP} ${PORT_MAP} ${VOL_MAP} ${ENV_MAP} -d ${IMAGE}
 
 # sudo docker logs --follow gost
+# use sh i.s.o. bash
+# docker exec -it gost sh
