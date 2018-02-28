55022efbd5d2e42cfecefdf4fa4ebee298c32bb9
Just van den Broecke <just@justobjects.nl>
2018-02-28 15:38:21 +0100

    #87 make monitoring work via Docker Ubuntu 16.4

--- services/monitoring/run.sh
+++ services/monitoring/run.sh
@@ -13,6 +13,21 @@ envsubst < alertmanager/config.yml > alertmanager/config-gen.yml
 envsubst < prometheus/prometheus.yml > prometheus/prometheus-gen.yml
 
 echo "External URL is http://${se_host}${se_port}"
-node_exporter/start.sh
-cadvisor/start.sh
-docker-compose up -d
+
+UBUNTU_VERSION=`lsb_release -rs | cut -d'.' -f1`
+
+case ${UBUNTU_VERSION} in
+  14) echo "${UBUNTU_VERSION}"
+    export CADVISOR_HOST=${PARENT_HOST}
+	node_exporter/start.sh
+	cadvisor/start.sh
+	docker-compose -f docker-compose-14.04.yml -d up
+  ;;
+  16) echo "${UBUNTU_VERSION}"
+    export CADVISOR_HOST=cadvisor
+	docker-compose -d up
+  ;;
+  *) echo "${UBUNTU_VERSION} unsupported"; exit -1
+  ;;
+esac
+
--- services/monitoring/stop.sh
+++ services/monitoring/stop.sh
@@ -2,7 +2,20 @@
 #
 /bin/rm -f alertmanager/config-gen.yml  prometheus/prometheus-gen.yml
 
-docker-compose stop
-docker-compose rm -f
-node_exporter/stop.sh
-cadvisor/stop.sh
+UBUNTU_VERSION=`lsb_release -rs | cut -d'.' -f1`
+
+case ${UBUNTU_VERSION} in
+  14) echo "${UBUNTU_VERSION}"
+	docker-compose -f docker-compose-14.04.yml stop
+	docker-compose -f docker-compose-14.04.yml rm -f
+	node_exporter/stop.sh
+	cadvisor/stop.sh
+  ;;
+  16) echo "${UBUNTU_VERSION}"
+	docker-compose stop
+	docker-compose rm -f
+  ;;
+  *) echo "${UBUNTU_VERSION} unsupported"; exit -1
+  ;;
+esac
+
