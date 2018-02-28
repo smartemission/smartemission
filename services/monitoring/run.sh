34d34ac40f675d7035d46e957d63c63baea5a6de
Just van den Broecke <just@justobjects.nl>
2018-02-28 16:20:45 +0100

    #87 make monitoring work via Docker Ubuntu 16.4 - node-exporter

--- services/monitoring/run.sh
+++ services/monitoring/run.sh
@@ -1,3 +1,4 @@
+File Edit Options Buffers Tools Sh-Script Help
 #!/bin/bash
 #
 source ../../etl/options/`hostname`.args
@@ -8,26 +9,28 @@ export GMAIL_ACCOUNT
 export GMAIL_AUTH_TOKEN
 export PARENT_HOST=`ip route show | grep docker0 | awk '{print \$9}'`
 
+echo "External URL is http://${se_host}${se_port}"
+
+UBUNTU_VERSION=`lsb_release -rs | cut -d'.' -f1`
+echo "Ubuntu version: ${UBUNTU_VERSION}"
+
+case ${UBUNTU_VERSION} in
+    14) export CADVISOR_HOST=${PARENT_HOST}
+        export NODE_EXPORTER_HOST=${PARENT_HOST}
+        export DOCKER_COMPOSE_YML="docker-compose-14.04.yml"
+        node_exporter/start.sh
+        cadvisor/start.sh
+        ;;
+    16) export CADVISOR_HOST=cadvisor
+        export NODE_EXPORTER_HOST=${PARENT_HOST}
+        export DOCKER_COMPOSE_YML="docker-compose.yml"
+        ;;
+    *) echo "${UBUNTU_VERSION} unsupported"; exit -1
+       ;;
+esac
+
 # Substitute env vars in .yaml
 envsubst < alertmanager/config.yml > alertmanager/config-gen.yml
 envsubst < prometheus/prometheus.yml > prometheus/prometheus-gen.yml
 
-echo "External URL is http://${se_host}${se_port}"
-
-UBUNTU_VERSION=`lsb_release -rs | cut -d'.' -f1`
-
-case ${UBUNTU_VERSION} in
-  14) echo "${UBUNTU_VERSION}"
-    export CADVISOR_HOST=${PARENT_HOST}
-	node_exporter/start.sh
-	cadvisor/start.sh
-	docker-compose -f docker-compose-14.04.yml -d up
-  ;;
-  16) echo "${UBUNTU_VERSION}"
-    export CADVISOR_HOST=cadvisor
-	docker-compose -d up
-  ;;
-  *) echo "${UBUNTU_VERSION} unsupported"; exit -1
-  ;;
-esac
-
+docker-compose -f ${DOCKER_COMPOSE_YML} -d up
