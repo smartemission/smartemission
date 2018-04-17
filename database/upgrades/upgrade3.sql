b8cd5f3bded8705c293a34053df03b720ec6a8fb
Just van den Broecke <just@justobjects.nl>
2018-04-17 15:07:43 +0200

    #111 gost/gost-db#5 upgrade SQL script GOST DB 0.5 to 0.6

--- database/upgrades/upgrade3.sql
+++ database/upgrades/upgrade3.sql
@@ -1,6 +1,20 @@
-ALTER TABLE v1.featureofinterest ADD COLUMN geojson jsonb;
-ALTER TABLE v1.location ADD COLUMN geojson jsonb;
+--
+-- Upgrade GOST DB 0.5 --> 0.6
+-- Just van den Broecke - april 17, 2018
+--
 
+-- NEW geojson shadow column for feature column
+-- geojson column must be like {"type": "Point", "coordinates": [5.671321, 51.472583]}
+ALTER TABLE v1.featureofinterest ADD COLUMN geojson jsonb;
+UPDATE v1.featureofinterest SET geojson = concat('{"type": "Point", "coordinates": [' || ST_X(feature) || ', ' ||  ST_Y(feature) || ']}')::jsonb;
+
+
+-- NEW geojson shadow column for feature column
+ALTER TABLE v1.location ADD COLUMN geojson jsonb;
+UPDATE v1.location SET geojson = concat('{"type": "Point", "coordinates": [' || ST_X(feature) || ', ' ||  ST_Y(feature) || ']}')::jsonb;
+
+
+-- Optimizations for observation table
 ALTER TABLE v1.observation DROP CONSTRAINT observation_pkey;
 CREATE INDEX i_dsid_id
   ON v1.observation
