411da853fd3960795cf0dcd829ee5abe41d4b92c
Just van den Broecke <just@justobjects.nl>
2018-07-26 14:01:56 +0200

    restore script

---
+++ platform/restore-all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Routine backup for all data of the platform.
+#
+# Just van den Broecke - 2018
+#
+
+BACKUP_DIR=/var/smartem/backup
+
+echo "START restore PG databases on `date`"
+
+SCHEMAS="smartem_rt smartem_raw smartem_refined smartem_calibrated sos52n1 v1"
+for SCHEMA in ${SCHEMAS}
+do
+	BACKUP_FILE=${BACKUP_DIR}/gis-smartem_${SCHEMA}.dmp
+	./restore-db.sh ${SCHEMA}
+done
+
+echo "END restore PG databases op `date`"
+
+BACKUP_FILE="${BACKUP_DIR}/influxdb_smartemission_data.tar.gz"
+./../services/influxdb/restore.sh ${BACKUP_FILE}
