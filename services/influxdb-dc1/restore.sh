2314f74aaa99b2d43a8485b902e7abd143e07fc4
Just van den Broecke <just@justobjects.nl>
2018-08-22 13:32:03 +0200

    #139 renewed backup/restore influxdb containers

--- services/influxdb-dc1/restore.sh
+++ services/influxdb-dc1/restore.sh
@@ -16,9 +16,6 @@ then
     exit 1
 fi
 
-# Parameter ok
-./stop.sh
-
 SCRIPT_DIR=${0%/*}
 
 pushd ${SCRIPT_DIR}
@@ -65,4 +62,20 @@ popd
 
 
 # On RUNNING container named influxdb
+INFLUX_CMD="docker exec ${SE_CONTAINER_NAME} influx -host 127.0.0.1 -port 8086 -database ${INFLUXDB_DB} -username ${INFLUXDB_ADMIN_USER} -password ${INFLUXDB_ADMIN_PASSWORD} -execute "
+
+# First must delete database otherwise meta restore errors
+echo "DROP DATABASE ${INFLUXDB_DB}..."
+${INFLUX_CMD} "DROP DATABASE ${INFLUXDB_DB}"
+
+echo "Restore ${INFLUXDB_DB} from ${CONTAINER_BACKUP_DIR} ..."
 docker exec ${SE_CONTAINER_NAME} influxd restore -portable -db ${INFLUXDB_DB} -newdb ${INFLUXDB_DB} /backup/${INFLUXDB_DB}
+
+# Permissions are somehow not set, users should still exist
+echo "Restore permissions for users ${INFLUXDB_READ_USER} and ${INFLUXDB_WRITE_USER} ..."
+${INFLUX_CMD} "GRANT READ ON ${INFLUXDB_DB} TO ${INFLUXDB_READ_USER}"
+${INFLUX_CMD} "GRANT ALL ON ${INFLUXDB_DB} TO ${INFLUXDB_WRITE_USER}"
+
+# Cleanup
+echo "Removing unpacked backup in ${BACKUP_DIR} ..."
+rm -rf ${BACKUP_DIR}
--- services/influxdb-dc1/test.sh
+++ services/influxdb-dc1/test.sh
@@ -11,7 +11,7 @@ function query() {
   QUERY="$2"
   echo "db=$DB - query: ${QUERY}"
   # See https://docs.influxdata.com/influxdb/v1.4/guides/querying_data/
-  curl -G 'http://dc1.smartemission.nl:8086/query?pretty=true' \
+  curl -G 'http://localhost:8086/query?pretty=true' \
   -u ${INFLUXDB_READ_USER}:${INFLUXDB_READ_USER_PASSWORD} \
      --data-urlencode "db=${DB}" --data-urlencode \
      "q=${QUERY}"
@@ -23,14 +23,17 @@ function query() {
 #query _internal "SHOW DATABASES"
 #query _internal "SHOW USERS"
 
-query ${INFLUXDB_DB} "SELECT * from ASE_NL_01 limit 2"
-query ${INFLUXDB_DB} "SELECT * from ASE_NL_02 limit 2"
-query ${INFLUXDB_DB} "SELECT * from ASE_NL_03 limit 2"
-query ${INFLUXDB_DB} "SELECT * from ASE_NL_04 limit 2"
-query ${INFLUXDB_DB} "SELECT * from ASE_NL_05 limit 2"
+# On RUNNING container named influxdb
+INFLUX_CMD="docker exec ${SE_CONTAINER_NAME} influx -host 127.0.0.1 -port 8086 -database ${INFLUXDB_DB} -username ${INFLUXDB_READ_USER} -password ${INFLUXDB_READ_USER_PASSWORD} -execute "
+
+${INFLUX_CMD} "SELECT * from ASE_NL_01 limit 2"
+${INFLUX_CMD} "SELECT * from ASE_NL_02 limit 2"
+${INFLUX_CMD} "SELECT * from ASE_NL_03 limit 2"
+${INFLUX_CMD} "SELECT * from ASE_NL_04 limit 2"
+${INFLUX_CMD} "SELECT * from ASE_NL_05 limit 2"
 
 # Get the last N records
-query ${INFLUXDB_DB} "SELECT * from ASE_NL_04 WHERE time > now() - 1h ORDER BY time DESC LIMIT 21"
+${INFLUX_CMD} "SELECT * from ASE_NL_04 WHERE time > now() - 1h ORDER BY time DESC LIMIT 21"
 
 # examples
 # http://test.smartemission.nl:8086/query?db=smartemission&q=SELECT%20*%20from%20rivm%20limit%202
